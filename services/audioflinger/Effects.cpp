d70d9b6408d71fc58749d17a94a0c022a2f3af58
laijun <laijun@xiaomi.com>
2022-10-20 20:33:39 +0800

    [Audio][Spatializer][Bugfix] Clear entire chain buffer to avoid dirty data
    FUXI-9082
    
    * We unlock the limits of two channel spatiailzer effect
    which cause overlap noise due to chain input buffer clear
    only works for actual data size instead of entire buffer.
    the remain buffer will keep dirty data.
    
    Change-Id: I970352d039dcdc40232fd484e98af149025815fa
    Signed-off-by: laijun <laijun@xiaomi.com>

--- services/audioflinger/Effects.cpp
+++ services/audioflinger/Effects.cpp
@@ -2254,10 +2254,13 @@ void AudioFlinger::EffectChain::clearInputBuffer_l()
     if (mInBuffer == NULL) {
         return;
     }
-    const size_t frameSize = audio_bytes_per_sample(EFFECT_BUFFER_FORMAT)
-            * mEffectCallback->inChannelCount(mEffects[0]->id());
+    //const size_t frameSize = audio_bytes_per_sample(EFFECT_BUFFER_FORMAT)
+            //* mEffectCallback->inChannelCount(mEffects[0]->id());
 
-    memset(mInBuffer->audioBuffer()->raw, 0, mEffectCallback->frameCount() * frameSize);
+    // AUDIO MOD: We clear entire buffer instead of actual data size to avoid dirty data retention
+    // on spatializer thread.
+    // memset(mInBuffer->audioBuffer()->raw, 0, mEffectCallback->frameCount() * frameSize);
+    memset(mInBuffer->audioBuffer()->raw, 0, mInBuffer->getSize());
     mInBuffer->commit();
 }
 
